1444bcba05027c277d481e6d1b410e93a1e36aeb
Paul Graff <banana.manwich@gmail.com>
2020-02-29 12:30:04 -0500

    Added basic AI for patrolling different points on a map

---
+++ Assets/Scripts/EnemyAi/EnemyPatrolBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrolBehavior : MonoBehaviour
+{
+    public float minDistanceToCompletePatrol = 5.0f;
+
+    private int activePatrolPointIndex = 0;
+    private List<PatrolPointBehavior> patrolPoints = new List<PatrolPointBehavior>();
+    private NavMeshAgent navAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+
+        //Expecting the patrol points and the actual enemy pawn to be under a common root
+        patrolPoints.AddRange(transform.parent.gameObject.GetComponentsInChildren<PatrolPointBehavior>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(patrolPoints.Count <= 0)
+        {
+            //There's no points to patrol, so don't do anything.
+            return;
+        }
+
+        UpdateCurrentPatrolPoint();
+    }
+
+    private void UpdateCurrentPatrolPoint()
+    {
+        PatrolPointBehavior currentPoint = patrolPoints[activePatrolPointIndex];
+        
+        // Reached Patrol Point
+        if (IsAtLocation(currentPoint.gameObject.transform.position))
+        {
+            PatrolPointBehavior.ActionStatus status = currentPoint.DoPatrolReached();
+            if(status == PatrolPointBehavior.ActionStatus.COMPLETE || status == PatrolPointBehavior.ActionStatus.ERROR)
+            {
+                // Move on to the next point
+                activePatrolPointIndex = (activePatrolPointIndex + 1) % patrolPoints.Count;
+            }
+        }
+        else
+        {
+            navAgent.destination = currentPoint.gameObject.transform.position;
+        }
+    }
+
+
+    private bool IsAtLocation(Vector2 loc)
+    {
+        Vector2 currentLoc = gameObject.transform.position;
+
+        float distanceFromPoint = (loc - currentLoc).magnitude;
+
+        return minDistanceToCompletePatrol > distanceFromPoint;
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyAi/PatrolPointBehavior.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPointBehavior : MonoBehaviour
+{
+    public enum ActionStatus
+    {
+        NOT_STARTED,
+        COMPLETE,
+        IN_PROGRESS,
+        STARTED,
+        ERROR
+    }
+
+    public float waitTime = 4.0f;
+
+    private ActionStatus status = ActionStatus.NOT_STARTED;
+    private float accumulatedWaitTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Disabled by default. Activates when started. For performance so patrol points aren't ticking all the time
+        enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Default behavior to just wait around at the patrol point. Doing nothing interesting.
+        switch(status)
+        {
+            case ActionStatus.NOT_STARTED:
+            case ActionStatus.COMPLETE:
+            case ActionStatus.ERROR:
+                //Do Nothing in these states
+                break;
+            case ActionStatus.STARTED:
+                accumulatedWaitTime = 0.0f;
+                status = ActionStatus.IN_PROGRESS;
+                break;
+            case ActionStatus.IN_PROGRESS:
+                accumulatedWaitTime += Time.deltaTime;
+                if(accumulatedWaitTime > waitTime)
+                {
+                    status = ActionStatus.COMPLETE;
+                }
+                break;
+            default:
+                //Error! status is some weird unexpected number.
+                status = ActionStatus.ERROR;
+                break;
+        }
+    }
+
+    // Function called by the patrolling character. Will be called periodically. Kicks off and completes the behavior or will just say it's in progress.
+    public virtual ActionStatus DoPatrolReached()
+    {
+        if(status == ActionStatus.COMPLETE || status == ActionStatus.ERROR)
+        {
+            ActionStatus returnStatus = status;
+            ResetAction(); //Resets so ready to run again later.
+            return returnStatus; //Returning so the caller knows the action just ended.
+        }
+
+        // Kick off the action.
+        if(status == ActionStatus.NOT_STARTED)
+        {
+            
+            status = ActionStatus.STARTED;
+            enabled = true;
+            return status;
+        }
+
+        // Other states changed through the actual update behavior for the object
+        return status;
+    }
+
+    private void ResetAction()
+    {
+        enabled = false;
+        status = ActionStatus.NOT_STARTED;
+    }
+
+}
