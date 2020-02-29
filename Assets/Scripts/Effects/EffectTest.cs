0afa35217a6a9ebf4b9ac89bf15f23ef80d8d7f2
yusuken64 <61254107+yusuken64@users.noreply.github.com>
2020-02-29 13:55:52 -0500

    Add Effects

---
+++ Assets/Scripts/Effects/EffectTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectTest : MonoBehaviour
+{
+    public BurnEffect effect1;
+    public BurnEffect effect2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartDie();
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        effect1.Reset();
+        effect2.Reset();
+    }
+
+    private void StartDie()
+    {
+        StartCoroutine(StartDieRoutine());
+    }
+
+    private IEnumerator StartDieRoutine()
+    {
+        effect1.StartDie();
+        yield return new WaitForSeconds(1);
+        effect2.StartDie();
+    }
+}
